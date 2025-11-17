8c68fcdb7bcd87629f2f2d9bcb3b93e2213da2f4
Kazushi Konosu <kazushi@konosu.me>
2025-11-17 23:32:03 +0900

    feat: setup linter

---
+++ eslint.config.js
@@ -0,0 +1,11 @@
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import { defineConfig } from 'eslint/config';
+import prettier from 'eslint-plugin-prettier/recommended';
+
+export default defineConfig([
+  { ignores: ['dist'] },
+  js.configs.recommended,
+  tseslint.configs.recommended,
+  prettier,
+]);
